6143aa7967ebbeb39a7dd02bc8a4092c9b9a653a
FreakyCoder <kurayogun@gmail.com>
2020-08-05 16:23:05 +0300

    Initial commit

--- example/lib/BeautifulHorizontalList.js
+++ example/lib/BeautifulHorizontalList.js
@@ -1,11 +1,52 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image, FlatList } from "react-native";
+/**
+ * ? Local Imports
+ */
+import styles, { _iconContainer } from "./BeautifulHorizontalList.style";
 
 export default class BeautifulHorizontalList extends Component {
-  render() {
+  renderItem = (data) => {
+    const { item, index } = data;
+    const { imageSource, title, value, unit, primaryColor } = item;
+    const {
+      TextComponent = Text,
+      ImageComponent = Image,
+      itemContainerStyle,
+    } = this.props;
     return (
-      <View>
-        <Text> textInComponent </Text>
+      <View
+        key={item.title + index}
+        style={[styles.itemContainer, itemContainerStyle]}
+      >
+        <TextComponent style={styles.titleTextStyle}>{title}</TextComponent>
+        <View style={styles.imageContainer}>
+          <View style={_iconContainer(primaryColor)}>
+            <ImageComponent source={imageSource} style={styles.iconStyle} />
+          </View>
+        </View>
+        <View style={styles.textContainer}>
+          <TextComponent style={styles.valueTextStyle}>{value}</TextComponent>
+          <View style={styles.unitTextContainer}>
+            <TextComponent style={styles.unitTextStyle}>{unit}</TextComponent>
+          </View>
+        </View>
+      </View>
+    );
+  };
+
+  render() {
+    const { data } = this.props;
+    return (
+      <View style={styles.container}>
+        <FlatList
+          horizontal
+          data={data}
+          renderItem={this.renderItem}
+          contentInset={styles.contentInsetStyle}
+          contentContainerStyle={styles.contentContainerStyle}
+          {...this.props}
+        />
       </View>
     );
   }
--- example/lib/BeautifulHorizontalList.style.js
+++ example/lib/BeautifulHorizontalList.style.js
@@ -1,1 +1,79 @@
-export default {};
+export const _iconContainer = (primaryColor) => ({
+  width: 50,
+  height: 50,
+  borderRadius: 25,
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundColor: "#fdfdfd",
+  shadowRadius: 8,
+  shadowOpacity: 0.3,
+  shadowColor: primaryColor,
+  shadowOffset: {
+    width: 0,
+    height: 3,
+  },
+});
+
+export const _unitTextStyle = (primaryColor) => ({
+  fontWeight: "700",
+  color: primaryColor,
+});
+
+export default {
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  contentContainerStyle: {
+    flex: 1,
+    paddingBottom: 24,
+    alignItems: "center",
+  },
+  contentInsetStyle: {
+    right: 150,
+  },
+  itemContainer: {
+    width: 160,
+    height: 220,
+    paddingTop: 12,
+    marginLeft: 12,
+    paddingLeft: 16,
+    paddingRight: 16,
+    borderRadius: 12,
+    paddingBottom: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fdfdfd",
+    shadowRadius: 8,
+    shadowOpacity: 0.1,
+    shadowColor: "#757575",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+  },
+  imageContainer: {
+    marginTop: 24,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  textContainer: {
+    marginBottom: 12,
+    marginTop: "auto",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  titleTextStyle: {
+    color: "#9D9D9D",
+    fontWeight: "600",
+  },
+  valueTextStyle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  unitTextContainer: {
+    marginTop: 8,
+  },
+};
